cab1fd63a37ca3e680755bfcb20b6657965775bd
Abhi26Shah <shahabhishek26@gmail.com>
2020-02-24 21:04:32 +0530

    Youtube API Player

---
+++ app/src/main/java/com/example/youtubeplayer/Config.java
@@ -0,0 +1,10 @@
+package com.example.youtubeplayer;
+
+public final class Config {
+
+    private Config() {
+
+    }
+    public static final String YOUTUBE_API_KEY = "AIzaSyBLmWQL1t4345nioK-3CrtuysA5uwy4ppI";
+}
+
